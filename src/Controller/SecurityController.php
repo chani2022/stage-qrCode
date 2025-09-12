1141cf24b3c0ac1c7004c3866a37de64320f25f2
chani <chaniandrianarinaiv@gmail.com>
2025-09-12 15:43:54 +0200

    config: suppression du cles json_login dans security.yaml

---
+++ src/Controller/SecurityController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class SecurityController extends AbstractController
+{
+    #[Route('/api/login', name: 'app_login', methods: ['POST'])]
+    public function index(): JsonResponse
+    {
+        return $this->json([
+            'message' => 'Welcome to your new controller!',
+            'path' => 'src/Controller/SecurityController.php',
+        ]);
+    }
+}
