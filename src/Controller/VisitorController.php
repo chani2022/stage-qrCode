c9b769ff5dc4630a49eb012fa7cbfb90e2836c86
chani <chaniandrianarinaiv@gmail.com>
2025-09-15 14:17:01 +0200

    chrone: ajout de nelmio/cors-bundle

---
+++ src/Controller/VisitorController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
+
+#[IsGranted('ROLE_USER')]
+final class VisitorController extends AbstractController
+{
+    #[Route('/api/visitor', name: 'app_visitor', methods: ['GET'])]
+    public function index(): JsonResponse
+    {
+        return $this->json([
+            'message' => 'Welcome to your new controller!',
+            'path' => 'src/Controller/VisitorController.php',
+        ]);
+    }
+}
